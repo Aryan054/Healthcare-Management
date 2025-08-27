025a98e2259247deb4f8e73559781aa091674aa2
Aryan <aryanrathod791@gmail.com>
2025-08-27 21:12:13 +0530

    FIX: Add build script and finalize database dependencies

--- build.sh
+++ build.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Upgrade pip to the latest version
-
-
-# Install all dependencies from requirements.txt
 pip install -r requirements.txt
 
-# Run Django's collectstatic command to gather all static files
-# into the directory specified in settings.py (STATIC_ROOT)
 python manage.py collectstatic --no-input
 
-# Apply database migrations (optional, but good for some setups)
-# python manage.py migrate
+# THIS LINE IS ESSENTIAL FOR CREATING DATABASE TABLES
 python manage.py migrate
