458dedd8fee1dd9374ded3102c25c28c79072949
Aryan <aryanrathod791@gmail.com>
2025-11-23 23:10:09 +0530

    Fix: added vercel domain to ALLOWED_HOSTS

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-
-# THIS LINE IS ESSENTIAL
-python manage.py makemigrations
-python manage.py migrate
