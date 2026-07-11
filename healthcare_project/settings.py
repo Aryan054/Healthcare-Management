37c38e81fe410f07a28e41d058eb06d12c562391
Aryan054 <aryanrathod791@gmail.com>
2026-07-11 15:43:57 +0530

    Configure Cloudinary

--- healthcare_project/settings.py
+++ healthcare_project/settings.py
@@ -127,6 +127,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if os.environ.get('CLOUDINARY_URL'):
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+elif os.environ.get('CLOUDINARY_CLOUD_NAME'):
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+        'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+        'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    }
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
